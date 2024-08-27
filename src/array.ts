7e49e1fc0efceee0c4406a4a8a834ae3326b97c1
Chang <AresChang114@qq.com>
2024-08-27 15:11:16 +0800

    feat: 扩充 array 工具函数

--- src/array.ts
+++ src/array.ts
@@ -9,3 +9,144 @@ export function toArray<T>(array?: Nullable<Arrayable<T>>): Array<T> {
   array = array ?? []
   return Array.isArray(array) ? array : [array]
 }
+
+/**
+ * 将 `Arrayable<T>` 转换成 `Array<T>` 并拉平数组
+ *
+ * @category Array
+ * @example flattenArrayable([1, [2, 3]]) // => [1, 2, 3]
+ */
+export function flattenArrayable<T>(array?: Nullable<Arrayable<T | Array<T>>>): Array<T> {
+  return toArray(array).flat(1) as Array<T>
+}
+
+/**
+ * 合并数组并且拉平一层数组
+ *
+ * @category Array
+ * @example mergeArrayable(1, [2, 3]) // => [1, 2, 3]
+ */
+export function mergeArrayable<T>(...args: Nullable<Arrayable<T>>[]): Array<T> {
+  return args.flatMap(i => toArray(i))
+}
+
+/**
+ * 数组唯一化
+ *
+ * @category Array
+ * @example uniq([1, 2, 3, 1, 2, 3]) // => [1, 2, 3]
+ */
+export function uniq<T>(array: readonly T[]): T[] {
+  return Array.from(new Set(array))
+}
+
+/**
+ * 通过自定义函数唯一化数组
+ *
+ * @category Array
+ * @example uniqueBy([1, 2, 3, 1, 2, 3], (a, b) => a === b) // => [1, 2, 3]
+ */
+export function uniqueBy<T>(array: readonly T[], equalFn: (a: any, b: any) => boolean): T[] {
+  return array.reduce((acc: T[], cur: any) => {
+    const index = acc.findIndex((item: any) => equalFn(cur, item))
+    if (index === -1)
+      acc.push(cur)
+    return acc
+  }, [])
+}
+
+/**
+ * 获取数组最后一项
+ *
+ * @category Array
+ * @example last([1, 2, 3]) // => 3
+ */
+export function last(array: readonly []): undefined
+export function last<T>(array: readonly T[]): T
+export function last<T>(array: readonly T[]): T | undefined {
+  return at(array, -1)
+}
+
+/**
+ * 删除数组中的某一项
+ *
+ * @category Array
+ * @example remove([1, 2, 3], 2) // => [1, 3]
+ */
+export function remove<T>(array: T[], value: T): boolean {
+  if (!array)
+    return false
+  const index = array.indexOf(value)
+  if (index >= 0) {
+    array.splice(index, 1)
+    return true
+  }
+  return false
+}
+
+/**
+ * 获取数组中的某一项
+ *
+ * @category Array
+ * @example
+ * at([1, 2, 3], 1) // => 2
+ * at([1, 2, 3], -1) // => 3
+ */
+export function at(array: readonly [], index: number): undefined
+export function at<T>(array: readonly T[], index: number): T
+export function at<T>(array: readonly T[] | [], index: number): T | undefined {
+  const len = array.length
+  if (!len)
+    return undefined
+
+  if (index < 0)
+    index += len
+
+  return array[index]
+}
+
+/**
+ * 生成一个指定范围的数组
+ *
+ * @category Array
+ * @example
+ * range(10) // => [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ * range(1, 10, 2) // => [1, 3, 5, 7, 9]
+ */
+export function range(stop: number): number[]
+export function range(start: number, stop: number, step?: number): number[]
+export function range(...args: any): number[] {
+  let start: number, stop: number, step: number
+
+  if (args.length === 1) {
+    start = 0
+    step = 1;
+    ([stop] = args)
+  }
+  else {
+    ([start, stop, step = 1] = args)
+  }
+
+  const arr: number[] = []
+  let current = start
+  while (current < stop) {
+    arr.push(current)
+    current += step || 1
+  }
+
+  return arr
+}
+
+/**
+ * 移动数组中的元素
+ *
+ * @category Array
+ * @param arr
+ * @param from
+ * @param to
+ * @example move([1, 2, 3], 1, 0) // => [2, 1, 3]
+ */
+export function move<T>(arr: T[], from: number, to: number): T[] {
+  arr.splice(to, 0, arr.splice(from, 1)[0])
+  return arr
+}
--- test/array.test.ts
+++ test/array.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest'
-import { toArray } from '../src/array'
+import { at, flattenArrayable, last, mergeArrayable, move, range, remove, toArray, uniq, uniqueBy } from '../src/array'
 
-describe('toArray 函数测试', () => {
+describe('toArray 函数', () => {
   it('应将 null 转换为空数组', () => {
     expect(toArray(null)).toEqual([])
   })
@@ -20,5 +20,75 @@ describe('toArray 函数测试', () => {
     expect(toArray([1, 2, 3])).toEqual([1, 2, 3])
     expect(toArray(['a', 'b', 'c'])).toEqual(['a', 'b', 'c'])
     expect(toArray([{ key: 'value1' }, { key: 'value2' }])).toEqual([{ key: 'value1' }, { key: 'value2' }])
+  })
+})
+
+describe('flattenArrayable 函数', () => {
+  it('应该将嵌套数组拉平为一维数组', () => {
+    expect(flattenArrayable([1, [2, 3]])).toEqual([1, 2, 3])
+  })
+})
+
+describe('mergeArrayable 函数', () => {
+  it('应该合并多个数组并拉平一层', () => {
+    expect(mergeArrayable(1, [2, 3])).toEqual([1, 2, 3])
+    expect(mergeArrayable([1], [2], [3])).toEqual([1, 2, 3])
+  })
+})
+
+describe('uniq 函数', () => {
+  it('应该返回数组中的唯一元素', () => {
+    expect(uniq([1, 2, 3, 1, 2, 3])).toEqual([1, 2, 3])
+  })
+})
+
+describe('uniqueBy 函数', () => {
+  it('应该根据自定义函数返回唯一元素', () => {
+    expect(uniqueBy([1, 2, 3, 1, 2, 3], (a, b) => a === b)).toEqual([1, 2, 3])
+  })
+})
+
+describe('last 函数', () => {
+  it('应该返回非空数组的最后一个元素，空数组返回 undefined', () => {
+    expect(last([1, 2, 3])).toEqual(3)
+    expect(last([])).toEqual(undefined)
+  })
+})
+
+describe('remove 函数', () => {
+  it('应该从数组中移除指定元素，成功移除返回 true，否则返回 false', () => {
+    const arr = [1, 2, 3]
+    expect(remove(arr, 2)).toBe(true)
+    expect(arr).toEqual([1, 3])
+    expect(remove([], 1)).toBe(false)
+  })
+})
+
+describe('at 函数', () => {
+  it('应该根据索引返回数组中的元素，正索引和负索引都应正确处理，空数组返回 undefined', () => {
+    expect(at([1, 2, 3], 1)).toEqual(2)
+    expect(at([1, 2, 3], -1)).toEqual(3)
+    expect(at([], 0)).toEqual(undefined)
+  })
+})
+
+describe('range 函数', () => {
+  it('传入一个参数时，应该生成从 0 到指定值减一的数组', () => {
+    expect(range(5)).toEqual([0, 1, 2, 3, 4])
+  })
+
+  it('传入两个参数时，应该生成从起始值到结束值减一的数组', () => {
+    expect(range(1, 5)).toEqual([1, 2, 3, 4])
+  })
+
+  it('传入三个参数时，应该生成指定步长的数组', () => {
+    expect(range(1, 10, 2)).toEqual([1, 3, 5, 7, 9])
+  })
+})
+
+describe('move 函数', () => {
+  it('应该移动数组中的元素', () => {
+    expect(move([1, 2, 3], 1, 0)).toEqual([2, 1, 3])
+    expect(move([1, 2, 3], 0, 2)).toEqual([2, 3, 1])
   })
 })
