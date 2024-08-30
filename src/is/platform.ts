2a1f020688f4a22097cf250eb8cb6d775b124508
Chang <AresChang114@qq.com>
2024-08-30 15:33:12 +0800

    feat: 增加 Node 环境判断

--- src/is/platform.ts
+++ src/is/platform.ts
@@ -1,6 +1,29 @@
 import { is } from './types'
 
-// @ts-expect-error 检测 window
-export const isWindow = (val: any): boolean => typeof window !== 'undefined' && is(val, 'Window')
-// @ts-expect-error 检测 window
-export const isBrowser = typeof window !== 'undefined'
+/**
+ * 判断是否为 window
+ *
+ * @param val
+ * @returns 是否为 window
+ */
+export function isWindow(val: any): boolean {
+  return typeof window !== 'undefined' && is(val, 'Window')
+}
+
+/**
+ * 判断是否为浏览器环境
+ *
+ * @returns 是否为浏览器环境
+ */
+export function isBrowser(): boolean {
+  return typeof window !== 'undefined' && typeof window.document !== 'undefined'
+}
+
+/**
+ * 判断是否为 Node 环境
+ * @returns 是否为 Node 环境
+ */
+export function isNode(): boolean {
+  // eslint-disable-next-line node/prefer-global/process
+  return typeof process !== 'undefined' && process.versions !== undefined && process.versions.node !== undefined
+}
--- test/is/platform.test.ts
+++ test/is/platform.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { isBrowser, isWindow } from '../../src/is/platform'
+import { isBrowser, isNode, isWindow } from '../../src/is/platform'
 
 describe('is 平台判断函数测试', () => {
   it('isWindow 函数应正确判断 window 对象', () => {
@@ -7,6 +7,10 @@ describe('is 平台判断函数测试', () => {
   })
 
   it('isBrowser 函数应正确判断浏览器环境', () => {
-    expect(isBrowser).toBe(false)
+    expect(isBrowser()).toBe(false)
+  })
+
+  it('isNode 函数应正确判断 Node 环境', () => {
+    expect(isNode()).toBe(true)
   })
 })
